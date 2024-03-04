9b0e47ac6b7833ef552b86dfe832f2df9285095d
Tanveer <116706588+Tanveer143s@users.noreply.github.com>
2024-03-04 20:16:34 +0530

    feat: updated main.tf values

--- examples/github-oidc/main.tf
+++ examples/github-oidc/main.tf
@@ -14,7 +14,7 @@ locals {
 module "aws_github_oidc_role" {
   source = "../../modules/aws_github_oidc_role"
 
-  enable               = false
+  enable               = true
   environment          = local.environment
   name                 = local.name
   repository           = "terraform-aws-iam-role"

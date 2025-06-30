e6ce2ab0331c4ea13db223e31d90e37a11781399
cloudOpsgenius <tarunofficial781@gmail.com>
2025-06-30 23:37:31 +0530

    fix: replace deprecated resource and address aws provider version issue on linux_amd64

--- main.tf
+++ main.tf
@@ -20,13 +20,17 @@ resource "aws_iam_role" "default" {
   count                 = var.enabled ? 1 : 0
   name                  = module.labels.id
   assume_role_policy    = var.assume_role_policy
-  managed_policy_arns   = var.managed_policy_arns
   force_detach_policies = var.force_detach_policies
   path                  = var.path
   description           = var.description
   max_session_duration  = var.max_session_duration
   permissions_boundary  = var.permissions_boundary
   tags                  = module.labels.tags
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "default" {
+  role_name   = aws_iam_role.default[0].id
+  policy_arns = var.managed_policy_arns
 }
 
 ##----------------------------------------------------------------------------- 
